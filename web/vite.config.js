1e3900e6eef18d291e9b3c5db002256a3a05a422
R Sai Prasad <rsaiprasad@Mac.lan>
2025-08-22 18:29:21 +0530

    fix: correct path for static deploy

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,8 +1,10 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: './',  // <-- THIS IS CRUCIAL FOR RENDER
   build: {
     outDir: 'dist'
   }
